7abc207a7cc1f08ee90a3b70f77e2a712c0f6d45
NadaTarekk <nadat1429@gmail.com>
2023-09-18 01:15:27 +0200

    adding some css

--- src/pages/home/Post.js
+++ src/pages/home/Post.js
@@ -4,12 +4,12 @@ function Post({post}) {
     //do i need to use isLoading here?
 
   return (
-    <>
+    <div className="post">
         <Link to={`/post/${post.id}`} > <h2>{post.title}</h2> </Link> 
         <p>{post.datetime}</p>
         <p>{post.body}</p>
       
-    </>
+    </div>
   )
 }
 
